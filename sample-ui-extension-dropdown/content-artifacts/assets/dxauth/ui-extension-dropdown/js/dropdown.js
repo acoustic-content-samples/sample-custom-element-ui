25cd438cdf78fdf35ec8b241145056f2bf81ed75
Nipuna Weerasinghe <nipuweer@au1.ibm.com>
2018-07-03 11:28:28 +1000

    Updated the copyright notices in js files

--- sample-ui-extension-dropdown/content-artifacts/assets/dxauth/ui-extension-dropdown/js/dropdown.js
+++ sample-ui-extension-dropdown/content-artifacts/assets/dxauth/ui-extension-dropdown/js/dropdown.js
@@ -1,13 +1,19 @@
-/**
- * Copyright IBM Corp. 2018
- *
- * @description
- * Tiny Vanilla JavaScript dropdown styled as per the
- * dropdown selectors in the Watson Content Hub UI.
- *
- * @class
- * @param {(string|Object)} options.elem - HTML id of the select or the DOM element.
- */
+/*
+Copyright IBM Corporation 2018
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 var VanillaSelect = function(options) {
     var elem          = typeof options.elem === 'string' ?
         document.getElementById(options.elem) : options.elem,
